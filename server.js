39a93bde8eb8d1a16fe8291dbe9d69495fb18df7
Vinoth Siva M <75155074+sivastrue123@users.noreply.github.com>
2026-08-10 14:08:10 +0530

    Reorder POST_MESSAGE_ORIGIN environment variable

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 
 const PORT = 5000;
 const COLLABORA_URL = process.env.COLLABORA_URL ||  "https://ez-officeviewer-app.graycoast-78e47e4a.southindia.azurecontainerapps.io"||"http://localhost:9980" || "https://ez-officeviewer-wopi.azurewebsites.net" ;
-const POST_MESSAGE_ORIGIN = process.env.APP_ORIGIN || "http://localhost:8080" || "https://collabora-2wf8.onrender.com" ||"http://localhost:3000" || "https://demoapp.ezofis.com" || "https://v6app.ezofis.com";
+const POST_MESSAGE_ORIGIN = process.env.APP_ORIGIN || "https://collabora-2wf8.onrender.com" || "http://localhost:8080" ||"http://localhost:3000" || "https://demoapp.ezofis.com" || "https://v6app.ezofis.com";
 const CONVERT_TIMEOUT_MS = 180_000;   // big/scanned PDFs are slow
 const SAVE_DEBOUNCE_MS = 2_000;       // collapse rapid autosaves into one convert
 
